65796abddde807efc3cc576793b17ee15cde22b9
dan <dandan682@outlook.com>
2021-06-04 23:49:27 -0500

    update hijos.js

--- DOM/hijos.js
+++ DOM/hijos.js
@@ -7,7 +7,14 @@ description.innerHTML = 'Lista de cursos'
 const items = document.querySelectorAll('li:nth-child(even')
 for (let i = 0; i < items.length; i++) {
     let element = items[i]
-    element.style.background = '#f2f2f2'
+    element.style.background = '#c5c0f7'
 }
 
 const element = document.querySelector('div.row > ul.list-group > li')
+const list = document.querySelector('ul')
+
+console.log(list.childElementCount)
+console.log(list.children)
+for (let i = 0; i < list.children.length; i++) {
+    console.log(list.children[i])
+}
