ac753cb94f50ebf7ab4d08aa96e57972e2384541
dan <dandan682@outlook.com>
2021-06-10 00:12:56 -0500

    update hermanos.js

--- DOM/hermanos.js
+++ DOM/hermanos.js
@@ -67,11 +67,22 @@ const div_row = document.querySelector('.row')
 const div_container = document.querySelector('.container')
 const body = document.querySelector('body')
 
-element.addEventListener('click', funcition() {
-    console.log('Elemento:')
-})
+// element.addEventListener('click', show_messages) 
+// list.addEventListener('click', show_messages) 
+// div_row.addEventListener('click', show_messages) 
+// div_container.addEventListener('click', show_messages) 
+// body.addEventListener('click', show_messages) 
 
+for(let element of document.querySelectorAll('*')) {
+    element.addEventListener('click', show_messages)
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Hola Mundo!')
-})
+function show_messages(e) {
+    console.log('Elemento actual (this):', this.tagName)
+    console.log('Elemento que disparo el evento (e.target):', e.target.tagName)
+    console.log("\n")
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('Hola Mundo!')
+// })
