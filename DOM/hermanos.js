f6340663228ca8d60da7fe76d50d5e3a8d8f024d
dan <dandan682@outlook.com>
2021-06-10 12:11:24 -0500

    update hermanos.js

--- DOM/hermanos.js
+++ DOM/hermanos.js
@@ -34,6 +34,32 @@ title_form.addEventListener('change', function() {
 })
 
 function create_card(title, description) {
+    console.log('Create card', title, description)
+    let div = document.createElement('div')
+    div.classname = 'col'
+    let thumbnail = document.createElement('div')
+    thumbnail.classname = 'thumbnail'
+    let caption = document.createElement('div')
+    caption.classname = 'caption'
+    let h3 = document.createElement('h3')
+    h3.textContent = title
+    let p1 = document.createElement('p')
+    p1.textContent = description
+    let p2 = document.createElement('p')
+    let a = document.createElement('a')
+    a.classname = 'btn'
+
+    p2.appendChild(a)
+    caption.appendChild(h3)
+    caption.appendChild(p1)
+    caption.appendChild(p2)
+
+    thumbnail.appendChild(caption)
+    div.appendChild(thumbnail)
+    row.appendChild(div)
+}
+
+function create_card_by_InnerHTML(title, description) {
     console.log("create_card")
     let html = `<div class= "col">\
         <div class="thumbnail">\
